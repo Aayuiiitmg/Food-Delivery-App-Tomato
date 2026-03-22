25d9ec81e0dbf212fe1b7bef841cde09fe40efbd
Aayuiiitmg <03aayushshankar9b@gmail.com>
2026-03-22 18:29:34 +0530

    vercel issues

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,11 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     // We are using the new 'testuser' and the 'Direct Mode' link to be 100% safe
-   const directUri = "mongodb+srv://Aayush_Shankar:591969@cluster0.don9obk.mongodb.net/food-del?retryWrites=true&w=majority&appName=Cluster0";
+   const directUri = "mongodb+srv://Aayush_Shankar:591969@cluster0.don9obk.mongodb.net/food-del?retryWrites=true&w=majority"
 
     try {
+
+        mongoose.set('bufferCommands', false);
         await mongoose.connect(directUri, {
             family: 4, // Forces IPv4
             tlsAllowInvalidCertificates: true, // Bypasses local Windows SSL blocks
